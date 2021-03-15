dd9158c4ce1abf8f2d6e3b097d53a0a0cf57e7fa
Sean Hodges <sthodges@gmail.com>
2021-03-15 16:45:24 -0700

    Fill in vals

--- wsgi_1.py
+++ wsgi_1.py
@@ -19,13 +19,15 @@ def application(environ, start_response):
     import pprint
     pprint.pprint(environ)
 
+    cur_time = datetime.datetime.now()
+
     response_body = body.format(
         software=environ.get('SERVER_SOFTWARE', default),
-        path="aaaa",
-        month="bbbb",
-        date="cccc",
-        year="dddd",
-        client_ip="eeee"
+        path=environ.get('PATH_INFO', default),
+        month=cur_time.strftime('%B'),
+        date=cur_time.strftime('%d'),
+        year=cur_time.strftime('%Y'),
+        client_ip=environ.get('REMOTE_ADDR', default)
     )
     status = '200 OK'
 

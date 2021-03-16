fb412e1be2b09e52d99de3095d43ab0dab4969aa
Sean Hodges <sthodges@gmail.com>
2021-03-16 11:13:52 -0700

    Break routing out to application() and handle errors

--- bookapp.py
+++ bookapp.py
@@ -20,7 +20,7 @@ def resolve_path(path):
     }
     path = path.strip('/').split('/')
     try:
-        return handlers[path[0]](*path[1:])
+        return handlers[path[0]], path[1:]
     except KeyError:
         raise NameError
 
@@ -28,9 +28,20 @@ def resolve_path(path):
 def application(environ, start_response):
     status = "200 OK"
     headers = [('Content-type', 'text/html')]
+
+    try:
+        path, args = resolve_path(environ.get('PATH_INFO'))
+        status = "200 OK"
+        body = path(*args)
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>404 Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>500 Internal Server Error</h1>"
     start_response(status, headers)
 
-    return [resolve_path(environ.get('PATH_INFO')).encode('utf8')]
+    return [body.encode('utf8')]
 
 
 if __name__ == '__main__':
